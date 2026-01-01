b580a8a296d06eb8d27a8dfb826fda6431b23473
dhgatjeye <195902293+dhgatjeye@users.noreply.github.com>
2026-01-01 15:17:13 +0300

    remove: no more need for tps

--- src/servers/sonoyuncu/config.rs
+++ src/servers/sonoyuncu/config.rs
@@ -1,5 +1,3 @@
 pub const PROCESS_NAME: &str = "javaw.exe";
 pub const SERVER_NAME: &str = "Sonoyuncu";
-pub const SERVER_TICK_RATE: u16 = 20; // 20 TPS = 50ms per tick
-pub const NETWORK_LATENCY_AVG: u16 = 20; // Average latency to Sonoyuncu servers (ms)
 pub const DESCRIPTION: &str = "Sonoyuncu optimized settings and configurations.";
